14ad510aefc9598862590793fe59856b3b8e3a27
dutaintegramy-crypto <dutaintegramy@gmail.com>
2026-09-12 22:17:08 +0800

    Add privacy and terms pages with use client directive

---
+++ src/app/[locale]/privacy/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy page.</p>
+    </div>
+  );
+}
---
+++ src/app/[locale]/terms/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Terms() {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>This is the terms of service page.</p>
+    </div>
+  );
+}
